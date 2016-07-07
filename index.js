5b2e3cad940cc86047d56d7ae5eb74369ca6e8cd
John Shanley <john.michael.shanley@gmail.com>
2016-07-07 01:38:57 -0400

    add density-ui to the includePaths

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+var path = require('path')
+
 var bourbonPaths = require('bourbon').includePaths
 var resetPath = require('node-reset-scss').includePath
 
 // copy bourbonPaths and then push the resetPath onto the end
 var includePaths = bourbonPaths.slice()
 includePaths.push(resetPath)
+
+// also include the density-ui lib itself
+includePaths.push(path.join(__dirname, 'lib'))
 
 exports.includePaths = includePaths
